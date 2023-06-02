82fbdfc4c4f44b7d0b3dcbb6cb541601c82139ef
Shawn <44221603+smarshall-spitzbart@users.noreply.github.com>
2023-06-02 12:13:03 -0700

    runs, fails, woot

--- main_test.go
+++ main_test.go
@@ -9,6 +9,23 @@ import (
 )
 
 func TestICSKeys(t *testing.T) {
-	require.NotEmpty(t, consumertypesv1.StoreKey)
-	require.NotEmpty(t, consumertypesv2.RouterKey)
+
+	require.Equal(t, consumertypesv1.PortByteKey, consumertypesv2.PortByteKey)
+	require.Equal(t, consumertypesv1.LastDistributionTransmissionByteKey, consumertypesv2.LastDistributionTransmissionByteKey)
+	require.Equal(t, consumertypesv1.UnbondingTimeByteKey, consumertypesv2.UnbondingTimeByteKey)
+	require.Equal(t, consumertypesv1.ProviderClientByteKey, consumertypesv2.ProviderClientByteKey)
+	require.Equal(t, consumertypesv1.ProviderChannelByteKey, consumertypesv2.ProviderChannelByteKey)
+	require.Equal(t, consumertypesv1.PendingChangesByteKey, consumertypesv2.PendingChangesByteKey)
+	require.Equal(t, consumertypesv1.PendingDataPacketsByteKey, consumertypesv2.PendingDataPacketsByteKey)
+	require.Equal(t, consumertypesv1.PreCCVByteKey, consumertypesv2.PreCCVByteKey)
+	require.Equal(t, consumertypesv1.InitialValSetByteKey, consumertypesv2.InitialValSetByteKey)
+	// missing key!!!!!! v2 branch is incorrect
+	// require.Equal(t, consumertypesv1.LastStandaloneHeightByteKey, consumertypesv2.LastStandaloneHeightByteKey)
+	require.Equal(t, consumertypesv1.SmallestNonOptOutPowerByteKey, consumertypesv2.SmallestNonOptOutPowerByteKey)
+	require.Equal(t, consumertypesv1.HistoricalInfoBytePrefix, consumertypesv2.HistoricalInfoBytePrefix)
+	require.Equal(t, consumertypesv1.PacketMaturityTimeBytePrefix, consumertypesv2.PacketMaturityTimeBytePrefix)
+	require.Equal(t, consumertypesv1.HeightValsetUpdateIDBytePrefix, consumertypesv2.HeightValsetUpdateIDBytePrefix)
+	require.Equal(t, consumertypesv1.OutstandingDowntimeBytePrefix, consumertypesv2.OutstandingDowntimeBytePrefix)
+	require.Equal(t, consumertypesv1.PendingDataPacketsBytePrefix, consumertypesv2.PendingDataPacketsBytePrefix)
+	require.Equal(t, consumertypesv1.CrossChainValidatorBytePrefix, consumertypesv2.CrossChainValidatorBytePrefix)
 }
